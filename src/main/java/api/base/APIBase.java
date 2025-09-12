0fe92ee07b4a1d687e43106831f10e1e20787029
omascorro <omar.mascorro@protonmail.com>
2025-09-11 18:47:25 -0600

    Creatin API project

---
+++ src/main/java/api/base/APIBase.java
@@ -0,0 +1,74 @@
+package api.base;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class APIBase {
+
+    private static final Logger logger = LoggerFactory.getLogger(APIBase.class);
+
+    protected String baseUrl;
+
+    public APIBase(String baseUrl) {
+        this.baseUrl = baseUrl;
+        useRelaxedHTTPSValidation();
+
+    }
+
+    public Response get (String endpoint){
+
+        Response response = given().contentType(ContentType.JSON)
+                .when().log().all().get(baseUrl + endpoint);
+
+                logger.info("Response: " +response.asString());
+
+                return response;
+    }
+
+    public Response post (String endpoint , Object body){
+
+        Response response = given().contentType(ContentType.JSON)
+                .body(body).when().log().all().post(baseUrl + endpoint);
+
+                logger.info("Response: " +response.asString());
+
+                return response;
+
+    }
+
+    public  Response put (String endpoint, Object body, String token){
+
+            Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON)
+                    .cookie("token",token).body(body).when().log().all().put(baseUrl + endpoint);
+
+
+                return response;
+    }
+
+    public  Response delete (String endpoint,String token){
+
+        Response response = given().contentType(ContentType.JSON)
+                .cookie("token",token).when().log().all().delete(baseUrl+endpoint);
+
+                return  response;
+    }
+
+    public Response uploadFile(String endpoint , File file){
+
+        Response response = given().contentType("multipart/form-data")
+                .accept(ContentType.JSON)
+                .multiPart("file",file)
+                .post(baseUrl+endpoint);
+
+        return response;
+
+    }
+
+
+}
---
+++ src/test/java/tests/BookingTest.java
@@ -0,0 +1,84 @@
+package tests;
+
+import static io.restassured.RestAssured.*;
+
+import api.booker.BookingAPI;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pojo.Booking;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BookingTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookingTest.class);
+
+
+    static BookingAPI bookingAPI;
+    static int bookingId;
+
+    @BeforeAll
+    static void beforeAll() {
+        bookingAPI = new BookingAPI();
+
+    }
+    /*
+    @Test
+    void getBooking() {
+
+        Response response = given().contentType(ContentType.JSON)
+                .when().log().all().get("https://restful-booker.herokuapp.com/booking/1");
+        logger.info(response.asString());
+
+        Assertions.assertNotNull(response);
+
+        Assertions.assertEquals(200,response.statusCode(),"Get Booking Status Code is not 200");
+
+
+    }
+
+     */
+
+    @Test
+    @Order(1)
+    void createBooking() {
+
+        Booking booking = new Booking("Teresa","Lopez",126,true,
+                "2025-10-10","2025-10-17","Lunch");
+
+        Response response = bookingAPI.createBooking(booking);
+
+        Assertions.assertEquals(200,response.statusCode(),"Failed: Status code was not 200.");
+
+        bookingId = response.jsonPath().getInt("bookingid");
+        logger.info("bookingId: "+bookingId);
+        Assertions.assertTrue(bookingId>0,"Failed: Booking Id should be greater than 0.");
+
+        String firstName = response.jsonPath().getString("booking.firstname");
+        logger.info("firstName: "+firstName);
+        Assertions.assertEquals(booking.getFirstname(),firstName,"Failed: firstName is incorrect.");
+
+        String lastName = response.jsonPath().getString("booking.lastname");
+        logger.info("lastName: "+lastName);
+        Assertions.assertEquals(booking.getLastname(),lastName,"Failed: lastName is incorrect.");
+
+        int totalPrice = response.jsonPath().getInt("booking.totalprice");
+        logger.info("totalPrice: "+totalPrice);
+        Assertions.assertEquals(booking.getTotalprice(),totalPrice,"Failed: Price is incorrect.");
+    }
+
+    @Test
+    @Order(2)
+    void getBooking() {
+
+        Response response = bookingAPI.getBooking(bookingId);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(200,response.statusCode(),"Failed: Status code was not 200.");
+
+        String firstName = response.jsonPath().getString("firstname");
+        logger.info("firstname:" + firstName);
+        Assertions.assertEquals("Teresa",firstName,"Failed: Firstname is not correct ");
+    }
+}
